a13e2d062e1543cee62eae21389fb4a7ce88bbb0
HIdeSSH <68917105+4hidessh@users.noreply.github.com>
2020-12-27 15:15:19 +0700

    Update tunnel1.sh

--- tunnel1.sh
+++ tunnel1.sh
@@ -25,16 +25,25 @@ wget https://git.io/vpnsetup -O vpnsetup.sh && sudo sh vpnsetup.sh
 
 
 #add remove account VPN lt2p
-wget -O /usr/local/bin/l2tp-add-user "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/l2tp-add-user"
-wget -O /usr/local/bin/l2tp-remove-user "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/l2tp-remove-user"
+#wget -O /usr/local/bin/l2tp-add-user "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/l2tp-add-user"
+#wget -O /usr/local/bin/l2tp-remove-user "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/l2tp-remove-user"
 
 # shared key
-wget -O /usr/local/bin/l2tp-get-psk "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/l2tp-get-psk"
+#wget -O /usr/local/bin/l2tp-get-psk "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/l2tp-get-psk"
 
 #permition
-chmod +x /usr/local/bin/l2tp-add-user
-chmod +x /usr/local/bin/l2tp-remove-user
-chmod +x /usr/local/bin/l2tp-get-psk
+#chmod +x /usr/local/bin/l2tp-add-user
+#chmod +x /usr/local/bin/l2tp-remove-user
+#chmod +x /usr/local/bin/l2tp-get-psk
+
+
+wget -O /usr/local/bin/stdev-l2tp-get-psk "https://gist.githubusercontent.com/satriaajiputra/0ddfe41a9218afb36f09d657cdfe8d91/raw/stdev-l2tp-get-psk"
+wget -O /usr/local/bin/stdev-l2tp-add-user "https://gist.githubusercontent.com/satriaajiputra/0ddfe41a9218afb36f09d657cdfe8d91/raw/stdev-l2tp-add-user"
+wget -O /usr/local/bin/stdev-l2tp-remove-user "https://gist.githubusercontent.com/satriaajiputra/0ddfe41a9218afb36f09d657cdfe8d91/raw/stdev-l2tp-remove-user"
+
+chmod +x /usr/local/bin/stdev-l2tp-get-psk 
+chmod +x /usr/local/bin/stdev-l2tp-add-user 
+chmod +x /usr/local/bin/stdev-l2tp-remove-user 
 
 #hapus script
 rm -rf tunnel1.sh
