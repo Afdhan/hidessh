9a0c86935ced610cd4cacdf09deccd109c6a82a9
HIdeSSH <68917105+4hidessh@users.noreply.github.com>
2020-12-27 16:51:10 +0700

    Update tunnel1.sh

--- tunnel1.sh
+++ tunnel1.sh
@@ -15,7 +15,7 @@ wget https://git.io/vpnsetup -O vpnsetup.sh && sudo sh vpnsetup.sh
 #wget https://git.io/vpnsetup -O vpnsetup.sh && sudo \
 #VPN_IPSEC_PSK='hidessh' \
 #VPN_USER='hidessh' \
-#VPN_PASSWORD='apagunanyahidup' \
+#VPN_PASSWORD='hidessh' \
 #sh vpnsetup.sh
 
 
@@ -35,10 +35,12 @@ wget https://git.io/vpnsetup -O vpnsetup.sh && sudo sh vpnsetup.sh
 wget -O /usr/local/bin/stdev-l2tp-get-psk "https://gist.githubusercontent.com/satriaajiputra/0ddfe41a9218afb36f09d657cdfe8d91/raw/stdev-l2tp-get-psk"
 wget -O /usr/local/bin/stdev-l2tp-add-user "https://gist.githubusercontent.com/satriaajiputra/0ddfe41a9218afb36f09d657cdfe8d91/raw/stdev-l2tp-add-user"
 wget -O /usr/local/bin/stdev-l2tp-remove-user "https://gist.githubusercontent.com/satriaajiputra/0ddfe41a9218afb36f09d657cdfe8d91/raw/stdev-l2tp-remove-user"
-
+#permission
 chmod +x /usr/local/bin/stdev-l2tp-get-psk 
 chmod +x /usr/local/bin/stdev-l2tp-add-user 
 chmod +x /usr/local/bin/stdev-l2tp-remove-user 
+#edit SHARED IpSEC
+wget -O /etc/ipsec.secrets "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/ipsec.secrets"
 
 
 #installer auto SSH, Dropbear , Stunnel, badVPN
