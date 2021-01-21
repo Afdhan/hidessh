0f9b27a1332c044923c1032d987d235c5d180fe2
HIdeSSH <68917105+4hidessh@users.noreply.github.com>
2021-01-21 14:11:05 +0700

    Update tunnel1.sh

--- tunnel1.sh
+++ tunnel1.sh
@@ -10,48 +10,26 @@ apt-get install wget curl -y
 
 wget https://git.io/vpnsetup -O vpnsetup.sh && sudo sh vpnsetup.sh
 
-#atau 
-#auto installer L2TP/Ipsec PSk 
-#wget https://git.io/vpnsetup -O vpnsetup.sh && sudo \
-#VPN_IPSEC_PSK='hidessh' \
-#VPN_USER='hidessh' \
-#VPN_PASSWORD='hidessh' \
-#sh vpnsetup.sh
 
-
-#add remove account VPN lt2p
-#wget -O /usr/local/bin/l2tp-add-user "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/l2tp-add-user"
-#wget -O /usr/local/bin/l2tp-remove-user "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/l2tp-remove-user"
-
-# shared key
-#wget -O /usr/local/bin/l2tp-get-psk "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/l2tp-get-psk"
-
-#permition
-#chmod +x /usr/local/bin/l2tp-add-user
-#chmod +x /usr/local/bin/l2tp-remove-user
-#chmod +x /usr/local/bin/l2tp-get-psk
-
-
+#tambahan konfigurasi
 wget -O /usr/local/bin/stdev-l2tp-get-psk "https://gist.githubusercontent.com/satriaajiputra/0ddfe41a9218afb36f09d657cdfe8d91/raw/stdev-l2tp-get-psk"
 wget -O /usr/local/bin/stdev-l2tp-add-user "https://gist.githubusercontent.com/satriaajiputra/0ddfe41a9218afb36f09d657cdfe8d91/raw/stdev-l2tp-add-user"
 wget -O /usr/local/bin/stdev-l2tp-remove-user "https://gist.githubusercontent.com/satriaajiputra/0ddfe41a9218afb36f09d657cdfe8d91/raw/stdev-l2tp-remove-user"
+
 #permission
 chmod +x /usr/local/bin/stdev-l2tp-get-psk 
 chmod +x /usr/local/bin/stdev-l2tp-add-user 
 chmod +x /usr/local/bin/stdev-l2tp-remove-user 
+
 #edit SHARED IpSEC
 wget -O /etc/ipsec.secrets "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/ipsec.secrets"
 
 #installer auto SSH, Dropbear , Stunnel, badVPN
 cd
-wget https://raw.githubusercontent.com/4hidessh/sshtunnel/master/debian10/ssh-baru.sh
-chmod +x ssh-baru.sh
-./ssh-baru.sh
+wget https://raw.githubusercontent.com/4hidessh/sshtunnel/master/debian10/ssh-baru.sh && chmod +x ssh-baru.sh && ./ssh-baru.sh
 
-#hapus script
-rm -rf tunnel1.sh
-rm -rf vpnsetup.sh
 
+cd
 #add dns adguard
 apt-get install resolvconf -y
 wget -O /etc/resolvconf/resolv.conf.d/head "https://raw.githubusercontent.com/4hidessh/sshtunnel/master/dns" && chmod +x /etc/resolvconf/resolv.conf.d/head
@@ -65,4 +43,6 @@ wget https://raw.githubusercontent.com/4hidessh/sshtunnel/master/firewall-torent
 netfilter-persistent save
 netfilter-persistent reload 
 
-
+#hapus script
+rm -rf tunnel1.sh
+rm -rf vpnsetup.sh
