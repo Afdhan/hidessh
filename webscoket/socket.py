7644047b1d5055e136f31be6155fc6c3c4c7c002
HIdeSSH <68917105+4hidessh@users.noreply.github.com>
2021-07-07 15:53:40 +0700

    Update socket.py

--- webscoket/socket.py
+++ webscoket/socket.py
@@ -12,11 +12,7 @@ PASS = ''
 BUFLEN = 4096 * 4
 TIMEOUT = 60
 DEFAULT_HOST = '127.0.0.1:143'
-else
-DEFAULT_HOST
-else
-DEFAULT_HOST
-RESPONSE = 'HTTP/1.1 101 WebSocket Di Izinkan\r\nContent-Length: 104857600000\r\n\r\n'
+RESPONSE = 'HTTP/1.1 101 WebSocket bye HideSSH\r\nContent-Length: 104857600000\r\n\r\n'
 
 class Server(threading.Thread):
     def __init__(self, host, port):
