158f265bf77cd14eb7ee33af84bd8a06e16511e9
HIdeSSH <68917105+4hidessh@users.noreply.github.com>
2021-07-07 19:06:27 +0700

    Update install-ws.sh

--- webscoket/install-ws.sh
+++ webscoket/install-ws.sh
@@ -6,13 +6,13 @@ cd
 
 #Install Script Websocket-SSH Python
 wget -O /usr/local/bin/ws-openssh https://raw.githubusercontent.com/4hidessh/hidessh/main/webscoket/openssh-socket.py
-#wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-dropbear && chmod +x /usr/local/bin/ws-dropbear
+wget -O /usr/local/bin/ws-dropbear https://raw.githubusercontent.com/4hidessh/hidessh/main/webscoket/dropbear-ws.py
 #wget -O /usr/local/bin/ws-stunnel https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-stunnel && chmod +x /usr/local/bin/ws-stunnel
 #wget -O /usr/local/bin/ws-ovpn https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-ovpn && chmod +x /usr/local/bin/ws-ovpn
 
 #izin permision
 chmod +x /usr/local/bin/ws-openssh
-#chmod +x /usr/local/bin/ws-dropbear
+chmod +x /usr/local/bin/ws-dropbear
 #chmod +x /usr/local/bin/ws-stunnel
 #chmod +x /usr/local/bin/ws-ovpn
 
@@ -21,7 +21,7 @@ chmod +x /usr/local/bin/ws-openssh
 wget -O /etc/systemd/system/ws-openssh.service https://raw.githubusercontent.com/4hidessh/hidessh/main/webscoket/service-wsopenssh && chmod +x /etc/systemd/system/ws-openssh.service
 
 #System Dropbear Websocket-SSH Python
-#wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-dropbear.service && chmod +x /etc/systemd/system/ws-dropbear.service
+wget -O /etc/systemd/system/ws-dropbear.service https://raw.githubusercontent.com/4hidessh/hidessh/main/webscoket/service-wsdropbear && chmod +x /etc/systemd/system/ws-dropbear.service
 
 #System SSL/TLS Websocket-SSH Python
 #wget -O /etc/systemd/system/ws-stunnel.service https://raw.githubusercontent.com/${GitUser}/test1/${namafolder}/main/ws-stunnel.service && chmod +x /etc/systemd/system/ws-stunnel.service
@@ -37,10 +37,10 @@ systemctl enable ws-openssh.service
 systemctl start ws-openssh.service
 systemctl restart ws-openssh.service
 
-#Enable & Start & Restart ws-openssh service
-#systemctl enable ws-dropbear.service
-#systemctl start ws-dropbear.service
-#systemctl restart ws-dropbear.service
+#Enable & Start & Restart ws-dropbear service
+systemctl enable ws-dropbear.service
+systemctl start ws-dropbear.service
+systemctl restart ws-dropbear.service
 
 #Enable & Start & Restart ws-openssh service
 #systemctl enable ws-stunnel.service
